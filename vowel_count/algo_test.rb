05214801ae68c166b233be646d29167d42cad5f9
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2020-06-16 14:51:16 -0600

    vowel count code war

---
+++ vowel_count/algo_test.rb
@@ -0,0 +1,21 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative 'algo'
+
+class AlgoTest < Minitest::Test
+  def test_it_exists
+    algo = Algo.new
+    assert_instance_of Algo, algo
+  end
+
+  def test_it_can_join_string
+    algo = Algo.new
+    assert_equal "abcd", algo.space_delete("ab cd")
+  end
+
+  def test_it_can_count_number_of_vowles
+    algo = Algo.new
+    string = "ab cd"
+    assert_equal 1, algo.getCount(string)
+  end
+end 
