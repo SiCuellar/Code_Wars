e6eb4ff67ecda84383f896c1305541925c77a49e
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2020-06-24 10:25:38 -0600

    awesome way to swap letters

--- Ruby/complementary_dna/algo.rb
+++ Ruby/complementary_dna/algo.rb
@@ -1,5 +1,23 @@
 require "pry"
 
 class Algo
-  
+  def DNA_strand(dna)
+    dict = {
+      "A" => "T",
+      "T" => "A",
+      "G" => "C",
+      "C" => "G"
+    }
+
+    dna.chars.map do |char|
+      dict[char]
+    end.join
+  end
 end
+
+
+
+
+# def DNA_strand(dna)
+#   dna.tr("ACTG", "TGAC")
+# end
--- Ruby/complementary_dna/algo_test.rb
+++ Ruby/complementary_dna/algo_test.rb
@@ -7,4 +7,18 @@ class AlgoTest < Minitest::Test
     algo = Algo.new
     assert_instance_of Algo, algo
   end
+
+  def test_it_can_form_complement_for_single
+    algo = Algo.new
+    assert_equal "T", algo.DNA_strand("A")
+    assert_equal "A", algo.DNA_strand("T")
+    assert_equal "G", algo.DNA_strand("C")
+    assert_equal "C", algo.DNA_strand("G")
+  end
+
+  def test_it_can_form_comeplement_for_full_sequence
+    algo = Algo.new
+    assert_equal "TAACG", algo.DNA_strand("ATTGC")
+    assert_equal "CATA", algo.DNA_strand("GTAT")
+  end
 end
