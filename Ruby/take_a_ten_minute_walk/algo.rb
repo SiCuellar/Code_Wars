a7cf3181ed6efa561500d48fa1c821e6cbc99a82
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2020-07-08 13:06:28 -0600

    fancy short way to solve this gs

--- Ruby/take_a_ten_minute_walk/algo.rb
+++ Ruby/take_a_ten_minute_walk/algo.rb
@@ -1,24 +1,29 @@
 require 'pry'
 
 class Algo
-  def is_valid_walk(walk)
-    if walk.count != 10
-      false
-    else
-      check_different_directions(walk)
-    end
-  end
-
-  def check_different_directions(directions)
-    counts = Hash.new(0)
-    directions.each do |dir|
-      counts[dir] += 1
-    end
-
-    if counts.values.uniq.count <= 1
-      true
-    else
-      false
-    end
+  def isValidWalk(walk)
+    walk.count('w') == walk.count('e') and
+    walk.count('n') == walk.count('s') and
+    walk.count == 10
   end
 end
+# def is_valid_walk(walk)
+#   if walk.count != 10
+#     false
+#   else
+#     check_different_directions(walk)
+#   end
+# end
+#
+# def check_different_directions(directions)
+#   counts = Hash.new(0)
+#   directions.each do |dir|
+#     counts[dir] += 1
+#   end
+#
+#   if counts.values.uniq.count <= 1
+#     true
+#   else
+#     false
+#   end
+# end
