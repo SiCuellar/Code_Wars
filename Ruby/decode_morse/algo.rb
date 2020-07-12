a14d9dbb39b94a3435fd357c4a49ae2b4e8d548e
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2020-07-12 09:55:47 -0700

    added deconde morse

---
+++ Ruby/decode_morse/algo.rb
@@ -0,0 +1,9 @@
+require 'pry'
+
+#test/Hash Provided
+class Algo
+  morse_letters = morseCode.split
+   morse_letters.map do |let|
+     MORSE_CODE[let]
+   end
+end
--- Ruby/decode_morse/alog_test.rb
+++ Ruby/decode_morse/alog_test.rb
@@ -3,5 +3,7 @@ require 'minitest/pride'
 require_relative 'algo'
 
 class AlgoTest < Minitest::Test
-
+  def test_it_exists
+    #Test Provided
+  end
 end
