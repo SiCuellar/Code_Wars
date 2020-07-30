7d7218d8b2765529eba9396e56ef9e6312991b63
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2020-07-30 14:55:33 -0700

    added sum dig pow method that works

--- Ruby/sum_dig_pow/algo.rb
+++ Ruby/sum_dig_pow/algo.rb
@@ -1,5 +1,21 @@
 require 'pry'
 
 class Algo
-  
+  def get_square_sum(num)
+    sum = 0
+    num_ints = num.to_s.split(//).map{|chr| chr.to_i}
+    num_ints.each_with_index do |num, i|
+      sum += num**(i+1)
+    end
+    sum
+  end
+
+  def sum_dig_pow(num_first, num_last)
+    collection = (num_first..num_last).to_a
+    collection.map do |num|
+      if num == get_square_sum(num)
+        num
+      end
+    end.compact
+  end
 end
--- Ruby/sum_dig_pow/algo_test.rb
+++ Ruby/sum_dig_pow/algo_test.rb
@@ -12,4 +12,14 @@ class AlgoTest < Minitest::Test
     algo = Algo.new
     assert_equal 89, algo.get_square_sum(89)
   end
+
+  def test_it_can_get_sum_of_raised_power_with_larger_nums
+    algo = Algo.new
+    assert_equal 2, algo.get_square_sum(11)
+  end
+
+  def test_it_can_check_to_see_if_sums_are_equal
+    algo = Algo.new
+    assert_equal [1, 2, 3, 4, 5, 6, 7, 8, 9], algo.sum_dig_pow(1,10)
+  end
 end
