689de328d0f9aef4b5693745e548bcf6cbac975c
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2020-08-03 20:53:36 -0600

    added fancy solutions gs

--- Ruby/create_phone_num/algo.rb
+++ Ruby/create_phone_num/algo.rb
@@ -8,3 +8,15 @@ class Algo
     "("+nums_1+")" + " " + nums_2 + "-" + nums_3
   end
 end
+
+
+
+
+# def createPhoneNumber(array)
+#   '(%d%d%d) %d%d%d-%d%d%d%d' % array
+# end
+
+
+# def createPhoneNumber(str)
+#   "(#{str[0..2].join}) #{str[3..5].join}-#{str[6..10].join}"
+# end
